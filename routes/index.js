c1eb06b24b8f6f54160c3825e0c203df374a9564
Shaunick Mistry <shaunickmistry@hotmail.co.uk>
2019-09-19 16:52:07 +0100

    Make request to ElastiSearch

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
-let express = require('express');
-let bodyParser = require("body-parser");
-let visearch = require('visearch-javascript-sdk');
-let router = express.Router();
+const express = require('express');
+const bodyParser = require("body-parser");
+const visearch = require('visearch-javascript-sdk');
+const rp = require('request-promise');
+const router = express.Router();
 
 /* Here we are configuring express to use body-parser as middle-ware. */
 router.use(bodyParser.urlencoded({ extended: false }));
@@ -11,40 +12,90 @@ router.use(bodyParser.json());
 visearch.set('app_key', '890640f4b381cbcb3b5cafb0b179c097');
 
 /* Test for individual image with Visense API. */
-router.get('/image-search', function(request, response, next) {
-  // return res.send('POST HTTP method on user resource: ' + image);
+router.get('/image-search', function(req, response, next) {
   visearch.uploadsearch({
-    im_url: "http://example.com/example.jpg",
+    im_url: "https://cdn-images.article.com/products/SKU2128/2890x1500/image46788.jpg?fit=max&w=2600&q=60&fm=webp",
   }, function(res) {
-      response.json(res);
+      /* Collect related SKUs from Visenze API call. */
+      let SKUs = extractSKUs(res.result);
+
+      /* Get product information from ElastiGraph. */
+      getElastiInformation(SKUs)
+          .then(function (parsedBody){
+            response.send(extractProductData(parsedBody));
+          });
+
   }, function(err){
-    /**
-     *{message:"Failed to fetch"}
-     **/
-    response.send("Err")
+      response.send("GET error")
   });
 });
 
-/* POST. */
-router.post('/image-search', function(req, res, next) {
-  let image = req.body.test;
-  // return res.send('POST HTTP method on user resource: ' + image);
+function extractSKUs(list) {
+  let SKUs = [];
+  list.forEach(function(item){
+    SKUs.push(item.im_name);
+  });
+  return SKUs;
+}
+
+function getElastiInformation(SKUs) {
+  let skuList = "";
+
+  SKUs.forEach(function(sku){
+    skuList += "\""+ sku + "\",";
+  });
+
+  const elastigraph_query = {
+    query: "{  products(skus: [" + skuList + "], store: GB) { sku name price { current } details images { listingImage } } }"
+  };
+
+  console.log(elastigraph_query);
+
+  let options = {
+    method: "POST",
+    uri: "https://es-elastigraph.made.com/graphql",
+    body: elastigraph_query,
+    json: true
+  };
+
+  return rp(options);
+}
+
+function extractProductData(productData) {
+  console.log(productData);
+  let matches = [];
+  productData.data.products.forEach(function(product){
+    matches.push({
+      SKU: product.sku,
+      description: product.name,
+      image_url: product.images.listingImage,
+      price: product.price.current,
+      style: product.details.style,
+      type: product.details.productType.value
+    });
+  });
+  return {
+    matches: matches
+  };
+}
+
+/* Search for related products given an image. */
+router.post('/image-search', function(request, response, next) {
+  let image = req.body.image;
   visearch.uploadsearch({
-    image: image,
-    fl: ["im_url","price"]
+      image: image,
+      fl: ["im_url","price"]
   }, function(res) {
-    if(res.status == "OK"){
-      return res.send(res);
-      // $(".main .similars").html(response_template(res));
-    }else{
-      // $(".main .similars").html(res.error);
-    }
-    // $(".submit .glyphicon").hide();
+      /* Collect related SKUs from Visenze API call. */
+      let SKUs = extractSKUs(res.result);
+
+      /* Get product information from ElastiGraph. */
+      getElastiInformation(SKUs)
+          .then(function (parsedBody){
+            response.send(extractProductData(parsedBody));
+          });
   }, function(err){
-    /**
-     *{message:"Failed to fetch"}
-     **/
-    // alert(err);
+      response.send("POST error")
   });
 });
 
